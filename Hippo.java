2efe1e94ddc8015a933a82726819c9f3d9c6e831
isabella7565 <101139102+isabella7565@users.noreply.github.com>
2022-05-20 14:11:19 -0400

    added idle, sound and movement left and right

--- Hippo.java
+++ Hippo.java
@@ -8,8 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hippo extends Actor
 {
-    //GreenfootSound hippoSound = new GreenfootSound("casinobling.mp3");
-    GreenfootImage[] idle = new GreenfootImage[8];
+    GreenfootSound hippoSound = new GreenfootSound("casinobling.mp3.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    String facing = "right";
     /**
      * Act - do whatever the Hippo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,21 +20,37 @@ public class Hippo extends Actor
     
     public Hippo()
     {
-        for(int i = 0; i < idle.length; i++)
+        for(int i = 0; i < idleRight.length; i++)
         {
-           idle[i] = new GreenfootImage("images/idle/idle" + i + ".png");
+           idleRight[i] = new GreenfootImage("images/idle/idle" + i + ".png.png");
         }
-            
-        setImage(idle[0]);
+        
+        for(int i = 0; i < idleLeft.length; i ++)
+        {
+           idleLeft[i] = new GreenfootImage("images/idle/idle" + i + ".png.png");
+           idleLeft[i].mirrorHorizontally();
+        }   
+        setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateHippo()
     {
-        setImage(idle[imageIndex]);
-        imageIndex = imageIndex + 1 % idle.length;
+       
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        
+        } 
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+            
+        
     }
-    
+   }
         
     
    
@@ -39,13 +58,15 @@ public class Hippo extends Actor
     {
         if (Greenfoot.isKeyDown("a")){
             move(-3);
+            facing = "left";
             // Add your action code here.
         } 
         if (Greenfoot.isKeyDown("d")){
             move(3);
+            facing = "right";
         }
         if (isTouching(Bomb.class)){
-            //hippoSound.play();
+            hippoSound.play();
             removeTouching(Bomb.class);
             MyWorld world = (MyWorld) getWorld();
             world.increaseScore();
